3d350ae14b82aa10ad178f0910327eb4061a3cf2
Nguyen Khoa <khoandhe160736@fpt.edu.vn>
2024-05-16 02:20:07 +0700

    update repository
    Add new repo

---
+++ Sourse/EXE_Dotel/Repository/Rental/RentalRepostiory.cs
@@ -0,0 +1,25 @@
+﻿
+using EXE_Dotel.Models;
+
+namespace EXE_Dotel.Repository.Rental
+{
+    public class RentalRepostiory : IRentalRepository
+    {
+        DotelDBContext dBContext;
+        public RentalRepostiory(DotelDBContext context) { dBContext = context; }
+        public Models.Rental GetRental(int id)
+        {
+            EXE_Dotel.Models.Rental? currentRental= dBContext.Rentals.FirstOrDefault(rental=> rental.RentalId == id);
+            if (currentRental != null)
+            {
+                return currentRental;
+            }
+            return currentRental;
+        }
+
+        public List<Models.Rental> GetRentals()
+        {
+            return dBContext.Rentals.ToList();
+        }
+    }
+}
---
+++ Sourse/EXE_Dotel/Repository/RentalImages/RentalImageRepository.cs
@@ -0,0 +1,12 @@
+﻿using EXE_Dotel.Models;
+
+namespace EXE_Dotel.Repository.RentalImages
+{
+    public class RentalImageRepository : iRentalImageRepository
+    {
+        public List<RentalListImage> GetListImageByRentalId(int rentalId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
