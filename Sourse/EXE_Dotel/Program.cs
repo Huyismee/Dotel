fc3aa64f9da8c3dd222783eee3d1848c0b91b414
Nguyen Khoa <khoandhe160736@fpt.edu.vn>
2024-05-16 16:34:00 +0700

    reconfig

--- Sourse/EXE_Dotel/Program.cs
+++ Sourse/EXE_Dotel/Program.cs
@@ -1,5 +1,6 @@
 using EXE_Dotel.Models;
 using EXE_Dotel.Repository.Rental;
+using EXE_Dotel.Repository.RentalImages;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddDbContext<DotelDBContext>(opt=> opt.UseSqlServer("server =localhost; database = DotelDB;uid=khoand;pwd=1234; trusted_connection = true; "));
 
 builder.Services.AddScoped<IRentalRepository, RentalRepostiory>();
+builder.Services.AddScoped<iRentalImageRepository, RentalImageRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
