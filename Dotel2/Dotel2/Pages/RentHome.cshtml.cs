1198e5c3ef9baa44fc6f026e44777db2e765c658
Nguyen Khoa <khoandhe160736@fpt.edu.vn>
2024-05-24 20:20:34 +0700

    add new page

---
+++ Dotel2/Dotel2/Pages/RentHome.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dotel2.Pages
+{
+    public class RentHomeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
