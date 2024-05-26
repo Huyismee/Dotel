282e5a5bc445bf9d2b7e98222536bd131314ac39
Nguyen Khoa <khoandhe160736@fpt.edu.vn>
2024-05-27 01:18:08 +0700

    Update DotelDBContext.cs

--- Dotel2/Dotel2/Models/DotelDBContext.cs
+++ Dotel2/Dotel2/Models/DotelDBContext.cs
@@ -30,7 +30,7 @@ namespace Dotel2.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("server =HUYISME\\SQLEXPRESS; database = DotelDB;uid=sa;pwd=123; trusted_connection = true; TrustServerCertificate=true ");
+                optionsBuilder.UseSqlServer("server =localhost; database = DotelDB;uid=khoand;pwd=1234; trusted_connection = true; TrustServerCertificate=true ");
             }
         }
 
